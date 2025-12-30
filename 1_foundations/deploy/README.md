494d38aaf026acb5f3c654767f0811f8a2523a03
Ashutosh Nigam <ashutosh.nigam@live.com>
2025-12-30 19:25:57 +0530

    deploy app

---
+++ 1_foundations/deploy/README.md
@@ -0,0 +1,7 @@
+---
+title: career_conversation
+app_file: app.py
+sdk: gradio
+sdk_version: 5.49.1
+
+---
--- 1_foundations/deploy/app.py
+++ 1_foundations/deploy/app.py
@@ -76,8 +76,8 @@ tools = [{"type": "function", "function": record_user_details_json},
 class Me:
 
     def __init__(self):
-        self.openai = OpenAI()
-        self.name = "Ed Donner"
+        self.openai = OpenAI(base_url="https://api.groq.com/openai/v1", api_key=os.getenv("GROQ_API_KEY"))
+        self.name = "Ashutosh Nigam"
         reader = PdfReader("me/linkedin.pdf")
         self.linkedin = ""
         for page in reader.pages:
@@ -116,7 +116,7 @@ If the user is engaging in discussion, try to steer them towards getting in touc
         messages = [{"role": "system", "content": self.system_prompt()}] + history + [{"role": "user", "content": message}]
         done = False
         while not done:
-            response = self.openai.chat.completions.create(model="gpt-4o-mini", messages=messages, tools=tools)
+            response = self.openai.chat.completions.create(model="llama3-8b-8192", messages=messages, tools=tools)
             if response.choices[0].finish_reason=="tool_calls":
                 message = response.choices[0].message
                 tool_calls = message.tool_calls
